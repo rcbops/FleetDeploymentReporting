a03022dd0c1c98d166543d89bf8bcbfb0c00b40d
Daniel Curran <daniel.curran@rackspace.com>
2018-09-08 22:04:33 +0000

    alerts directive/controller created and added to ui where API calls are made

---
+++ web/web/static/web/js/components/alerts.js
@@ -0,0 +1,33 @@
+function AlertsController($scope, $filter, $log) {
+    var self = this;
+
+    self.$onInit = function() {
+        self.alerts = [];
+    };
+
+    self.removeAlert = function(index) {
+        self.alerts.splice(index, 1)
+        if (self.alerts.length == 0) {
+            self.onNoAlerts({});
+        }
+    };
+
+    $scope.$on('notify', function(notification, args) {
+        if (args.target == self.target) {
+            self.alerts.push(args);
+            if (self.alerts.length == 1) {
+                self.onHasAlerts({});
+            }
+        }
+    });
+}
+
+angular.module('cloudSnitch').component('hxAlert', {
+    templateUrl: '/static/web/html/alert.html',
+    controller: ['$scope', '$filter', '$log', AlertsController],
+    bindings: {
+        target: '<',
+        onHasAlerts: '&',
+        onNoAlerts: '&'
+    }
+});
